1875c51546c0b9d17072880f59e000b061389d54
nikitabaronia <nikita baronia31@gmail.com>
2019-06-18 23:08:34 +0530

    Create DataTypeHandling.kt
    Added DataType Handling

---
+++ Kotlin Programming/app/src/main/java/com/example/nikitabaronia/helloworld/DataTypeHandling.kt
@@ -0,0 +1,17 @@
+package com.example.nikitabaronia.helloworld
+
+fun main(args : Array<String>){
+
+//    there is are dataType specifier in Kotlin,
+// it will take according to the values assigned
+    var a = 1;  //declared int
+
+    var b = 1.0 //declared float
+
+    var myString = "Hello World" //declared mutuable string
+
+    var secondString : String // declaring var as String which one can define later
+
+    val immutableString = "I am declared as final" //making it final through val
+    //immutableString = "hi"  //it can not be reassigned any value
+}
