283d92ba5f879093b51027c04e24682b1446fcb5
nikitabaronia <nikita baronia31@gmail.com>
2019-06-18 23:32:53 +0530

    Create FunctionCall.kt
     Added FUNCTIONS CLASS & STRING INTERPOLATION

---
+++ Kotlin Programming/app/src/main/java/com/example/nikitabaronia/helloworld/FunctionCall.kt
@@ -0,0 +1,51 @@
+package com.example.nikitabaronia.helloworld
+
+fun main(array: Array<String>){
+    var myString : String //declaring as String
+
+    myString = "Hi this is function calling tutorial"
+
+    //call the printName fn and pass the value
+    printName(myString)
+
+    //calling a method inside of a class
+    //create object of class #display
+    var displayObj = display()
+    displayObj.displayName(myString) //method called
+
+    //assign through class object
+    var displayNameObj = displayNameWithoutVar()
+    displayNameObj.name = "nikita baronia" //asigned name a value
+    displayNameObj.displayName()  //call the displayName method without passing any variable
+
+    //if we want to print name in main method without calling method
+    println("Hi I am called from main method " + displayNameObj.name)
+
+    // $ is an identifier which will extract values from class var
+    println("Hi I am called inside brackets ${displayNameObj.name}")
+
+}
+
+//create a function to print name
+fun printName(name:String){ // syntax is {{varName : dataType }}
+    println(name)
+}
+
+//declare a class and create a method inside it
+class display{
+
+    //declare function
+    fun displayName(name:String){
+        println("Hi I am inside a class "   + name)
+    }
+}
+
+//declare a class and create a method inside it
+class displayNameWithoutVar{
+
+    var name :String = ""
+    //declare function
+    fun displayName(){
+        println("Hi I am inside a class" +name)
+    }
+}
